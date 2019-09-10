2a6809e891cbc8ffe3b7e4f64429b162f749cc5d
Mohammad Ahmar <ahmar.mohammad77@gmail.com>
2019-09-10 16:55:46 -0400

    remove

--- src/main/java/com/revature/calc/CalcDriver.java
+++
@@ -1,9 +0,0 @@
-package com.revature.calc;
-
-public class CalcDriver {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
--- src/main/java/com/revature/calc/Calculator.java
+++
@@ -1,8 +0,0 @@
-package com.revature.calc;
-
-public class Calculator {
-
-	public double add(String s) {
-		return 0.0;
-	}
-}
